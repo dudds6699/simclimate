3f9813459e024f1b33001cfe1585572e0519fdb0
chris <chris.matthew.bodley@gmail.com>
2014-12-07 15:10:32 +0000

    ok here we have points weird thing maps like long and lat (y,x) and yet postgis like (x,y) mental note for things

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -4,19 +4,34 @@ class MapController < ApplicationController
 
   def getheatpoints
       #2354 should get 63
+      #city = City.find(2354)
+      city = City.all
+      points = []
+      
+      city.each do |item|
+          point = item.geom
+          
+          points << [point.y, point.x]
+          
+      end
+      
+      @answer = points.to_json
+      
+
+  end
+  
+  def movethepoints
+    #2354 should get 63
       city = City.find(2354)
       pt = city.geom
-      wind = Wind.where("ST_DWithin(geom, ST_GeomFromText('POINT(#{pt.x} #{pt.y})',4326), 1)").first
+      wind = Wind.where("ST_DWithin(geom, ST_GeomFromText('POINT(#{pt.x} #{pt.y})',4326), 1)").first.geom
       
     if wind then
-      @answer = wind.id
+      @answer = wind[0]
     else
       @answer = 0
     end
-  end
-  
-  def find_within_radius(pt, dist = 0.01)
-    return :conditions => "ST_DWithin(geom, ST_GeomFromText('POINT(#{pt.x} #{pt.y})', 4326), #{dist})"
+    
   end
   
 end
--- app/models/wind.rb
+++ app/models/wind.rb
@@ -1,2 +1,3 @@
 class Wind < ActiveRecord::Base
+   # GEO_FACTORY = RGeo::Geographic.spherical_factory(srid: 4326)
 end
